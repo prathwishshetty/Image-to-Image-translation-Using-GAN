829f6d8e9b50b4f1b2abd81735e7572a99ff2b18
anishnarkar <32675649+anishnarkar@users.noreply.github.com>
2019-11-04 17:21:55 -0500

    Update:2

--- Evaluation.py
+++ Evaluation.py
@@ -26,5 +26,43 @@ def dice(component1,component2):
 
     return ((2. * np.sum(gtruth * pred)) /(np.sum(gtruth) + np.sum(pred) + eps))
                 
-               
+def PA(component1,component2):
+    eps = 1e-6
+    pred = np.asarray(component1).astype(np.bool)
+    gtruth = np.asarray(component2).astype(np.bool)
+
+    if not np.sum(gtruth) + np.sum(pred):
+        return 1.0
+
+    return ((2. * np.sum(gtruth * pred)) /(np.sum(gtruth) + np.sum(pred) + eps))
+
+def calculate_hausdroff(component1,component2):
+    number_points = np.shape(component1)[0]
+    factor = 0
+    
+    for i in range(0,number_points):
+        factor = factor + max(directed_hausdorff(u, v)[i], directed_hausdorff(v, u)[i])
+        
+    factor = factor/number_points
+    return(factor)
+ 
+ def trimap(component1,component2):
+    intermediate_result = []
+    for row in range(0, 5):
+        for column in range(0,5):
+            intermediate_result = intermediate_result + [jaccard_similarity_score(component1[row][column],component2[row][column])]  
+            
+    return(np.mean(intermediate_result))
+
+def bfScore(component1,component2):
+    intermediate_result = -1
+    theta = 10
+    for k in range(0,theta+1):
+        a = np.add(component1,l)
+        for l in range(0,theta+1):
+            b = np.add(component2,l)
+            if intermediate_result > jaccard_similarity_score(component1[row][column],component2[row][column]):
+                intermediate_result = jaccard_similarity_score(component1[row][column],component2[row][column])
+                                                              
+    return(intermediate_result)
  
